335e3808018e4fabe563f8ac8027e60638ba0bb7
Rebecca <i123jj-118@yahoo.com.hk>
2024-11-19 17:34:21 +0800

    fuck

---
+++ 123.js
@@ -0,0 +1,40 @@
+document.addEventListener('DOMContentLoaded', () => {
+const navBtn = document.getElementById("nav-mobile-toggle");
+const navLinks = document.getElementById("mobile-nav-list");
+
+// navBtn.addEventListener("click", () => {
+//     navLinks.classList.toggle("active");
+// })
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+// add date
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+})
